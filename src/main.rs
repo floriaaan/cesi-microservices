21187e65c77a0e1539c329fe5213371ef98d6acd
Florian LEROUX <10078837+floriaaan@users.noreply.github.com>
2023-01-16 09:33:17 +0100

    feat(post): add post endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use rocket::serde::json::Json;
+use rocket::{ serde::json::Json};
+use serde::Deserialize;
 
 #[macro_use]
 extern crate rocket;
 
-#[derive(FromForm, Default)]
+#[derive(FromForm, Default, Deserialize)]
 pub struct SumParams {
     number1: Option<i32>,
     number2: Option<i32>,
 }
 
 #[get("/sum?<params..>")]
-fn index(params: SumParams) -> Json<i32> {
+fn get(params: SumParams) -> Json<i32> {
+    let number1 = params.number1.unwrap();
+    let number2 = params.number2.unwrap();
+    return Json(number1 + number2);
+}
+
+#[post("/sum", data = "<params>")]
+fn post(params: Json<SumParams>) -> Json<i32> {
     let number1 = params.number1.unwrap();
     let number2 = params.number2.unwrap();
     return Json(number1 + number2);
@@ -18,5 +26,5 @@ fn index(params: SumParams) -> Json<i32> {
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![get, post])
 }
